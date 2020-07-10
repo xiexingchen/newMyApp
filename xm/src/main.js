288d44ae9707bd490d8fa89e98d4f653ac1dca98
yourname <charlie2012@aliyun.com>
2020-07-10 17:22:44 +0800

    首页

---
+++ xm/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import "./style/rest.css"
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+Vue.config.productionTip = false
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
